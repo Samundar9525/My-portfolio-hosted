4a1c89a7e9ffc7d24066d2b0dad2eaedb564fa4a
samundar <samundarsingh9525@gmail.com>
2025-08-04 21:56:23 +0530

    correctes roles

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -2,7 +2,7 @@ import image from '../assets/images/image10.png';
 
 export const aboutData = {
   title: "Who I Am",
-  description1: "I am Samundar Singh, a Software Engineer with over 3 years of experience in frontend development, with a strong focus on Angular and UI architecture.",
+  description1: "I am Samundar Singh, a Software Engineer with over 3+ years of experience in Software development, with a strong focus on Python, Angular and UI architecture.",
   description2: "Originally from Jharkhand, I completed my undergraduate education there before earning my master’s degree from Anna University, Chennai. This academic journey opened the door to my current role at Caterpillar Inc., where I contribute to building scalable and maintainable web applications. My technical expertise spans Angular, TypeScript, JavaScript, Node.js, Python (Django), RESTful APIs, and both SQL (PostgreSQL) and NoSQL (MongoDB) databases. I am deeply committed to continuous learning, open-source contribution, and creating clean, reusable components that enhance performance and user experience. Outside of work, I have a strong interest in travel and cultural exploration, which also fuels my passion for discovering innovative food experiences.",
   image: 1,
   imageUrl: image
--- src/data/headerData.js
+++ src/data/headerData.js
@@ -3,8 +3,8 @@ import image from '../assets/images/image1.png'
 
 export const headerData = {
     name: 'Samundar Singh',
-    title: "Frontend Developer",
-    desciption:'I’m a Frontend Developer specializing in Angular and Material UI, currently working at Caterpillar Inc. I focus on building reusable components, enhancing modularization, and improving the efficiency of development processes.',
+    title: "Software Engineer",
+    desciption:'I’m a Software Engineer specializing in Angular and Python, currently working at Caterpillar Inc. I focus on building reusable components, enhancing modularization, and improving the efficiency of development processes.',
     image: image,
     resumePdf: resume
 }
